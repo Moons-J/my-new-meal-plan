b2ea830da7046fde356b7545991eeaf12d309800
jonasmoons <moons_jonas@outlook.com>
2023-11-29 17:28:07 +0100

    added agenda

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
+  # before_action :set_time_zone, if: :user_signed_in?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :user_name, :birthday, :height, :exercise_level, :weight ])
   end
+
+  # private
+
+  # def set_time_zone
+  #   Time.zone = current_user.time_zone
+  # end
 end
