62a08cfe81a77441a68864848b467c08cdbc0a78
Carl Busch <carl@123busch.de>
2023-11-29 11:08:53 +0100

    started working on nutrition calc methode

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,7 +1,15 @@
 class MealsController < ApplicationController
-skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
     @meals = Meal.all
+  end
+
+  def calculate_nutrition
+    calories = 0
+    self.ingredients.each do |ingredient|
+      calories += ingredient.calories
+    end
+    return calories
   end
 end
