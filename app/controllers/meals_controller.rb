310d17b7309375cd772fd0396f6becac655e14e3
Carl Busch <carl@123busch.de>
2023-11-29 13:08:39 +0100

    working on search feature

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -2,6 +2,7 @@ class MealsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
+    @query = params[:query]
     @meals = Meal.all
   end
 end
