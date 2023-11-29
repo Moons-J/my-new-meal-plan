0ec027e7173612dc7c5c7a602bc3a3a8cea258a2
Carl Busch <carl@123busch.de>
2023-11-29 16:27:09 +0100

    added ajax search to meals

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,8 +3,12 @@ class MealsController < ApplicationController
     user_meals = Meal.where(user: current_user)
     @query = params[:query]
     @meals = user_meals
-    if @query
+    if @query.present?
       @meals = user_meals.search_by_name_comment_ingredients(params[:query])
+    end
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: "meals/meals_list", formats: [:html] }
     end
   end
 end
