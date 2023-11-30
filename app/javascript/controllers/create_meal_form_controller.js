f4389998f31ee2c302d32d81ddbd5c9615338529
Valeriia Chavdar <valera.chavdar@mail.ru>
2023-11-30 14:10:12 +0100

    Select ingredients style(green border)

--- app/javascript/controllers/create_meal_form_controller.js
+++ app/javascript/controllers/create_meal_form_controller.js
@@ -2,11 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="create-meal-form"
 export default class extends Controller {
+  static targets = ["card", "amount"]
   connect() {
     console.log("Hello there");
   }
 
-  select() {
-    console.log("clicked");
+  select(event) {
+    console.log("event.target", event.target.tagName);
+
+    if (event.target.tagName !== "I" && event.target.tagName !== "INPUT") {
+      event.currentTarget.classList.toggle("selected");
+      if (event.currentTarget.classList.contains("selected")) {
+        event.currentTarget.querySelector("input").autofocus = true;
+      }
+    }
+
+    if (!event.currentTarget.classList.contains("selected")) {
+      this.amountTarget.value = "";
+    }
+  }
+
+  add() {
+    this.element.classList.add("selected");
   }
 }
