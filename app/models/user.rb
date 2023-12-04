241d17af87d359f1a9213c3675959aef46b8b12d
jonasmoons <moons_jonas@outlook.com>
2023-12-04 15:41:49 +0100

    fixed user macro calculation methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,23 +22,25 @@ class User < ApplicationRecord
 
   after_create :copy_base_ingredients
 
-  def user_calories(current_user)
-    user_nutri_calculation(current_user) * real_active_level(current_user) * user_phase(current_user)
+
+  def user_calories
+    total = self.user_nutri_calculation * self.real_active_level * self.user_phase
+    total.to_i
   end
 
-  def minimum_user_fats(current_user)
+  def min_user_fats(current_user)
     current_user.weight * 0.9 * 9
   end
 
-  def user_satu_fats(current_user)
+  def max_user_satu_fats(current_user)
     user_calories(current_user) * 0.1
   end
 
-  def user_carbs(current_user)
+  def max_user_carbs(current_user)
     user_calories(current_user) * 0.65
   end
 
-  def user_protein(current_user)
+  def min_user_protein(current_user)
     case current_user.phase
     when "gain"
       current_user.weight * 2 * 1.1
@@ -51,56 +53,59 @@ class User < ApplicationRecord
 
   def date_not_in_future
     errors.add(:birthday, "can't be in the future") if birthday.present? && birthday > Date.today
-    end
   end
 
   def copy_base_ingredients
     BaseIngredient.all.each do |ingredient|
       Ingredient.create(
-      name: ingredient.name,
-      calories: ingredient.calories,
-      fats: ingredient.fats,
-      satu_fats: ingredient.satu_fats,
-      carbs: ingredient.carbs,
-      protein: ingredient.protein,
-      user_id: self.id
-      )
+        name: ingredient.name,
+        calories: ingredient.calories,
+        fats: ingredient.fats,
+        satu_fats: ingredient.satu_fats,
+        carbs: ingredient.carbs,
+        protein: ingredient.protein,
+        user_id: self.id
+        )
+      end
     end
-  end
 
-  def user_phase(current_user)
-    case current_user.phase
-    when "gain"
-      phase = 1.1
-    when "maintain"
-      phase = 1
-    when "lose"
-      phase = 0.9
+    def age_year
+      Date.today.year - self.birthday.year
     end
-  end
 
-  def real_active_level(current_user)
-    case current_user.active_level
-    when 1
-      new_active_level = 1.2
-    when 2
-      new_active_level = 1.375
-    when 3
-      new_active_level = 1.55
-    when 4
-      new_active_level = 1.725
-    when 5
-      new_active_level = 1.9
-  end
+    def user_phase
+      case self.phase
+      when "gain"
+        phase = 1.1
+      when "maintain"
+        phase = 1.0
+      when "lose"
+        phase = 0.9
+      end
+    end
 
-  def user_nutri_calculation(current_user)
-    if current_user.sex == "male"
-      bmr = 66.5 + (13.75 * current_user.weight) + (5.003 * current_user.height) - (6.755 * current_user.age)
-    elsif current_user.sex == "female"
-      bmr = 655.1 + (9.563 * current_user.weight) + (1.85 * current_user.height) - (4.676 * current_user.age)
+    def real_active_level
+      case self.active_level
+      when 1
+        new_active_level = 1.2
+      when 2
+        new_active_level = 1.375
+      when 3
+        new_active_level = 1.55
+      when 4
+        new_active_level = 1.725
+      when 5
+        new_active_level = 1.9
+      end
+    end
+
+  def user_nutri_calculation
+    if self.sex == "male"
+      bmr = 66.5 + (13.75 * self.weight) + (5.003 * self.height) - (6.755 * self.age_year)
+    elsif self.sex == "female"
+      bmr = 655.1 + (9.563 * self.weight) + (1.85 * self.height) - (4.676 * self.age_year)
     else
-      bmr = 325 + (12 * current_user.weight) + (3.5 * current_user.height) - (6 * current_user.age)
+      bmr = 325 + (12 * self.weight) + (3.5 * self.height) - (6 * self.age_year)
     end
   end
-
 end
