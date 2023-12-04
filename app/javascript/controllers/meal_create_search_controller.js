965921e04d244e8d04e99a3c806ea1091c4939fc
Valeriia Chavdar <valera.chavdar@mail.ru>
2023-12-04 12:50:45 +0100

    Nutritions dynamic update

--- app/javascript/controllers/meal_create_search_controller.js
+++ app/javascript/controllers/meal_create_search_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="meal-create-search"
 export default class extends Controller {
-  static targets = ["input", "ingredient", "card", "list"];
+  static targets = ["input", "ingredient", "card", "list", "amount", "calloriesnum", "fatsnum", "satufatsnum", "carbsnum", "proteinnum"];
 
   connect() {
   }
@@ -22,6 +22,41 @@ export default class extends Controller {
       }
     });
 
+  }
+
+  calculate() {
+    if (this.cardTarget.classList.contains("selected") && !this.amountTarget.value==""){
+      // select all where amount is not empty
+      const ingredients = this.cardTargets.filter(card => !card.querySelector(".amount").value=="");
+      // calculate nutritions
+      let nutritions = {
+        calories: 0,
+        fats: 0,
+        satufats: 0,
+        carbs: 0,
+        protein: 0
+      }
+      // console.log(ingredients);
+      ingredients.forEach(ingredient => {
+        // console.log(parseFloat(ingredient.dataset.fats));
+        // console.log(parseFloat(ingredient.dataset.satufats ));
+        // console.log(parseFloat(ingredient.dataset.carbs ));
+        // console.log(parseFloat(ingredient.dataset.protein ));
+        // console.log(ingredient.querySelector(".amount").value);
+        let amount = parseInt(ingredient.querySelector(".amount").value)
+        nutritions.calories += parseInt(ingredient.dataset.callories) / 100 * amount;
+        nutritions.fats += parseFloat(ingredient.dataset.fats) / 100 * amount;
+        nutritions.satufats += parseFloat(ingredient.dataset.satufats) / 100 * amount;
+        nutritions.carbs += parseFloat(ingredient.dataset.carbs) / 100 * amount;
+        nutritions.protein += parseFloat(ingredient.dataset.protein) / 100 * amount;
+      });
+      // display nutritions
+      this.calloriesnumTarget.innerText = nutritions.calories;
+      this.fatsnumTarget.innerText = Math.round(nutritions.fats * 10) / 10;
+      this.satufatsnumTarget.innerText = Math.round(nutritions.satufats * 10) / 10;
+      this.carbsnumTarget.innerText = Math.round(nutritions.carbs * 10) / 10;
+      this.proteinnumTarget.innerText = Math.round(nutritions.protein * 10) / 10;
+    }
   }
 
   sort() {
