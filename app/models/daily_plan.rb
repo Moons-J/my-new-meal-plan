98ca6cc62b85469db13815c5303330094cbdc6dd
Carl Busch <carl@123busch.de>
2023-12-04 16:51:52 +0100

    done only styling left

--- app/models/daily_plan.rb
+++ app/models/daily_plan.rb
@@ -4,6 +4,7 @@ class DailyPlan < ApplicationRecord
   has_many :plannings
   has_many :daily_plan_meals
   has_many :meals, through: :daily_plan_meals
+  has_one_attached :photo
 
   accepts_nested_attributes_for :daily_plan_meals
 
