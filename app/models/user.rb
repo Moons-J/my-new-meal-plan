d2bbb2f460dbc1b1feabdd3b4afe4844355b5f03
jonasmoons <moons_jonas@outlook.com>
2023-12-04 11:41:06 +0100

    added new columns to user and methods in the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,18 +11,62 @@ class User < ApplicationRecord
   has_many :weight_histories
 
   validates :first_name, :last_name, :user_name, :birthday, :height, :exercise_level, :weight, presence: true
-# validation for exercise_level it should be between 1..5
+  # validation for exercise_level it should be between 1..5
   validates :exercise_level, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-# # validation for height it should be between 100..250
-#   validates :height, numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 250 }
-# # validation for weight it should be between 30..250
-#   validates :weight, numericality: { only_integer: true, greater_than_or_equal_to: 30, less_than_or_equal_to: 250 }
-# # validation for birthday it should be in the past
+  # # validation for height it should be between 100..250
+  #   validates :height, numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 250 }
+  # # validation for weight it should be between 30..250
+  #   validates :weight, numericality: { only_integer: true, greater_than_or_equal_to: 30, less_than_or_equal_to: 250 }
+  # # validation for birthday it should be in the past
   validate :date_not_in_future
 
   after_create :copy_base_ingredients
 
+  def user_calories(current_user)
+    user_nutri_calculation(current_user) * real_active_level(current_user)
+  end
+
+  def minimum_user_fats(current_user)
+    current_user.weight * 0.9 * 9
+  end
+
+  def user_satu_fats(current_user)
+    user_calories(current_user) * 0.1
+  end
+
+  def user_carbs(current_user)
+    user_calories(current_user) * 0.65
+  end
+
+  def user_protein(current_user)
+    user_calories(current_user) * 0.25 / 4
+  end
+
   private
+
+  def real_active_level(current_user)
+    case current_user.active_level
+    when 1
+      new_active_level = 1.2
+    when 2
+      new_active_level = 1.375
+    when 3
+      new_active_level = 1.55
+    when 4
+      new_active_level = 1.725
+    when 5
+      new_active_level = 1.9
+  end
+
+  def user_nutri_calculation(current_user)
+    if current_user.sex == "male"
+      bmr = 66.5 + (13.75 * current_user.weight) + (5.003 * current_user.height) - (6.755 * current_user.age)
+    elsif current_user.sex == "female"
+      bmr = 655.1 + (9.563 * current_user.weight) + (1.85 * current_user.height) - (4.676 * current_user.age)
+    else
+      bmr = 325 + (12 * current_user.weight) + (3.5 * current_user.height) - (6 * current_user.age)
+    end
+  end
 
   def date_not_in_future
     errors.add(:birthday, "can't be in the future") if birthday.present? && birthday > Date.today
@@ -41,4 +85,5 @@ class User < ApplicationRecord
       )
     end
   end
+end
 end
