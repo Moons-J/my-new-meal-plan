2c1cd4f91a2507ca32dfd876808bb93e2d2bde41
jonasmoons <moons_jonas@outlook.com>
2023-12-05 13:20:54 +0100

    merged master

--- app/controllers/account_controller.rb
+++
@@ -1,6 +0,0 @@
-class AccountController < ApplicationController
-  def edit
-    @user = current_user
-    
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_103110) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_092940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "base_ingredients", force: :cascade do |t|
     t.string "name"
