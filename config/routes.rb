d84f14fc5bff9a95303c075a9bb693af793da605
jonasmoons <moons_jonas@outlook.com>
2023-12-05 14:16:36 +0100

    merged master

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,5 @@ Rails.application.routes.draw do
   resources :meals, only: %i[index new create destroy]
   resources :daily_plans, only: %i[index new create]
   resources :ingredients, only: %i[index new create edit update destroy]
-<<<<<<< HEAD
-  resources :account, only: %i[edit update]
-=======
   resources :groceries, only: %i[index]
->>>>>>> master
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_092940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "accounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
