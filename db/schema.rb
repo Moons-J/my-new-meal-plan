799b020a5b5a5635a9b30de91ba3e93d985b699e
jonasmoons <moons_jonas@outlook.com>
2023-12-05 13:42:31 +0100

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_092940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-  create_table "accounts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-=======
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -45,7 +40,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_092940) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
->>>>>>> master
   end
 
   create_table "base_ingredients", force: :cascade do |t|
