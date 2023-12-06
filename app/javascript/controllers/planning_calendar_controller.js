33d9cdfbd373ee2506180f1fd1c9e6973cdcce43
luis sensat <luis.sensat@gmail.com>
2023-12-06 11:19:49 +0100

    update plans working

--- app/javascript/controllers/planning_calendar_controller.js
+++ app/javascript/controllers/planning_calendar_controller.js
@@ -3,13 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="planning-calendar"
 export default class extends Controller {
   static targets = ['form']
+
   connect() {
-    console.log("connected")
   }
   show(event){
     this.formTarget.classList.remove("d-none")
     const date = event.currentTarget.getAttribute("data-value")
     document.querySelector("#planning_date").setAttribute("value", date)
+    if (event.currentTarget.querySelector("i")) {
+      const planId = event.currentTarget.querySelector("i").getAttribute("data-plan-id")
+      fetch(`/daily_plans?p=${planId}`, { headers: { "Accept": "text/plain" } })
+        .then(response => response.text())
+        .then(data => {
+          this.formTarget.innerHTML = data
+        })
+    }
   }
 
   hide(){
