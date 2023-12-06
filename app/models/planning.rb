233900e69f7d9a4bf47f46f38b71201c70c80941
luis sensat <luis.sensat@gmail.com>
2023-12-06 12:22:46 +0100

    styling plan meals

--- app/models/planning.rb
+++ app/models/planning.rb
@@ -2,5 +2,7 @@ class Planning < ApplicationRecord
   belongs_to :daily_plan
   belongs_to :user
 
-  validates :date, uniqueness: true
+  validates :date, uniqueness: true, presence: true
+  validates :exercise, :boolean_field_name, inclusion: [true, false]
+  validates :daily_plan_id, presence: true
 end
