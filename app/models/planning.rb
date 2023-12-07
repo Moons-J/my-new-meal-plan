b52c746711d857d42970651ac059a93b08acdb13
Valeriia Chavdar <valera.chavdar@mail.ru>
2023-12-07 14:32:13 +0100

    validations for daily plan

--- app/models/planning.rb
+++ app/models/planning.rb
@@ -2,6 +2,15 @@ class Planning < ApplicationRecord
   belongs_to :daily_plan
   belongs_to :user
 
-  validates :date, uniqueness: true, presence: true
+  
   validates :daily_plan_id, presence: true
+  validate :unique_data_per_user
+
+  private
+
+  def unique_data_per_user
+    if Planning.where(date: date, user_id: user_id).exists?
+      errors.add(:date, "must be unique per user")
+    end
+  end
 end
