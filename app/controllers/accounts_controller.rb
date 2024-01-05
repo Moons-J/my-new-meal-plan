aa17c50c12bd34e9a6d64835b4eabfc38f26ff1c
Jonas Moons <moons_jonas@outlook.com>
2024-01-05 21:15:55 +0100

    removed exercise_level from accounts table

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,12 @@
 class AccountsController < ApplicationController
+
+  def new
+    @account = Account.new
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:user_id, :birthday, :height, :weight, :active_level, :phase)
+  end
 end
--- db/migrate/20240104143611_create_accounts.rb
+++ db/migrate/20240104143611_create_accounts.rb
@@ -2,7 +2,7 @@ class CreateAccounts < ActiveRecord::Migration[7.1]
   def change
     create_table :accounts do |t|
       t.integer :user_id, null: false
-      t.integer :height, :active_level, :exercise_level, null: false
+      t.integer :height, :active_level, null: false
       t.date :birthday, null: false
       t.float :weight, null: false
       t.string :sex, :phase, null: false
